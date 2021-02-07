e147e6ca5afb0787557f570aef4af40de7087254
Steve Dearth <stevedearth@us.ibm.com>
2021-02-07 11:17:52 -0500

    day 9

---
+++ day009/dictionary.py
@@ -0,0 +1,14 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.", 
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+empty_dictionary = {}
+
+print(programming_dictionary["Bug"])
+programming_dictionary["Loop"] = "The action of repeating an action over and over again."
+
+programming_dictionary["Bug"] = "A moth in your computer."
+print("-----")
+for key in programming_dictionary:
+    print(programming_dictionary[key])
---
+++ day009/visited.py
@@ -0,0 +1,14 @@
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited" : ["Paris, Lille, Dijon"],
+        "total_visits": 12
+    },
+    {
+        "country": "Germany",
+        "cities_visited" : ["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 10
+    }
+]
+
+print(travel_log[1])
